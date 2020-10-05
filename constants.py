cabad7211e02bc01c5ef84646ae53229bc85c8ba
boudalis <58219580+boudalis@users.noreply.github.com>
2020-10-05 17:54:20 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 13:32:56 2020
+
+@author: BOUDALIS
+"""
+
+url = 'https://alg-data-public.s3.amazonaws.com/'
+db_url='postgresql+psycopg2://postgres:postgres@algdb-1.c3kwj6orkqge.eu-west-3.rds.amazonaws.com:5432/algtst'
+tgt_table='alg_stg."Shopify_stg"'
+start_date = '2019-04-01'
+end_date = '2019-04-07'
