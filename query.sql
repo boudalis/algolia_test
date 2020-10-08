ffa4cf84185ae2f1e4426cafafe261cee1ac7ff7
boudalis <58219580+boudalis@users.noreply.github.com>
2020-10-08 16:41:15 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,3 +1,21 @@
+--create a database 
+CREATE DATABASE algtst
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE algtst
+    IS 'algtst';
+    
+--create a schema    
+CREATE SCHEMA alg_stg
+    AUTHORIZATION postgres;    
+
+--create a table 
 CREATE TABLE alg_stg."Shopify_stg"
 (
     id character varying(100) COLLATE pg_catalog."default",
